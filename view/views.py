7bee26245904f1a3de3e6e1163fbd248efaa4b04
sunil5391 <38095929+sunil5391@users.noreply.github.com>
2024-06-24 19:18:23 +0530

    Update views.py

--- view/views.py
+++ view/views.py
@@ -1,8 +1,8 @@
 from fastapi import status,HTTPException,APIRouter,Request
-from utils import get_current_date,validate_output_list,setup_logger
+from helper_functions import get_current_date,validate_output_list,setup_logger
 from model.api_request import ApiRequestModel
 from model.api_response import ApiResponseModel
-from controller.process_controller import execute_process
+from controller.process_controller import execute_adddition_procedure
 
 logger = setup_logger()
 router = APIRouter()
@@ -14,20 +14,19 @@ def additionIntegerslist(model: ApiRequestModel):
 	input_data=model.payload
 	logger.info('-----Batch id ------ {batchid} and {input_data}')
 	start_date=get_current_date()
-	output=execute_process(input_data)
+	output=execute_adddition_procedure(input_data)
 	if input_data is None:
 		logger.error('Input data is not available')
 		raise HTTPException(status_code=409,detail='not found')
 	elif validate_output_list(output):
-		logger.error('Output data is empty')
+		logger.error('Output data is blank')
 		raise HTTPException(status_code=418,detail='Empty output')
 	else:
-		logger.info(f"Processing is complete with {output}")
+		logger.info("Processing is complete with "+str(output))
 		ApiResponseModel.batchid=batchid
 		ApiResponseModel.response=output
 		ApiResponseModel.status="complete"
 		ApiResponseModel.started_at=start_date
 		ApiResponseModel.completed_at=get_current_date()
 		return ApiResponseModel
-            
             
